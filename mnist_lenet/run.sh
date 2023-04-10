ddccfb8c6c8d5289d24ab9d8a94acc5b6bb2a4c1
Thomas Humphries <t3humphr@uwaterloo.ca>
2023-04-10 23:43:44 +0000

    run script

--- mnist_lenet/run.sh
+++ mnist_lenet/run.sh
@@ -1,6 +1,8 @@
 export CUDA_VISIBLE_DEVICES=0
 
-for pop_size in 10 32 100 320 1000 3200
+for pop_size in 10 32 100 320 1000
 do
     python3 gaggle.py --config gaggle_mnist.yml --population_size $pop_size
+    python3 leap.py --population_size $pop_size
+    python3 pygad.py --population_size $pop_size
 done
