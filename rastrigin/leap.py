4296d397370c1a5ef4c54dafa0fc61ca95a0e8f3
Thomas Humphries <t3humphr@uwaterloo.ca>
2023-04-11 00:19:20 +0000

    buggy leap and pygad rastrigin

---
+++ rastrigin/leap.py
@@ -0,0 +1,76 @@
+"""An example of solving a reinforcement learning problem by using evolution to
+tune the weights of a neural network."""
+import time
+beginning_time = time.time()
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+sys.path.insert(1, os.path.join(sys.path[0], '../gaggle'))
+sys.path.insert(1, os.path.join(sys.path[0], '../LEAP'))
+sys.path.insert(1, os.path.join(sys.path[0], '../LEAP/leap_ec'))
+
+import torch
+
+
+
+
+from leap_ec import Individual, Representation, test_env_var, Decoder
+from leap_ec import probe, ops
+from leap_ec.algorithm import generational_ea
+
+from leap_ec.real_rep.initializers import create_real_vector
+from leap_ec.real_rep.problems import RastriginProblem
+from leap_ec.decoder import IdentityDecoder
+from new_leap_operators import TimingProbe, mutate_uniform, build_probes
+
+##############################
+# Entry point
+##############################
+if __name__ == '__main__':
+
+
+    # Parameters
+    runs_per_fitness_eval = 1
+    pop_size = 200
+    low = -1.
+    high = 1.
+    generations = 100
+    d = 4
+    # Load the OpenAI Gym simulation
+
+    # Representation
+
+    # Decode genomes into a feed-forward neural network,
+    # but also wrap an argmax around the networks so their
+    # output is a single integer
+    decoder = IdentityDecoder()
+    problem = RastriginProblem(d)
+    timing_probe = TimingProbe()
+    with open('./genomes.csv', 'w') as genomes_file:
+
+        ea = generational_ea(max_generations=generations, pop_size=pop_size,
+                            # Solve a problem that executes agents in the
+                            # environment and obtains fitness from it
+                            problem=problem,
+
+                            representation=Representation(
+                                initialize=create_real_vector(bounds=([(-5.12, 5.12)]*d)),
+                                decoder=decoder),
+
+                            # The operator pipeline.
+                            pipeline=[
+                                timing_probe,
+                                ops.proportional_selection,
+                                ops.clone,
+                                ops.uniform_crossover(p_xover=0.5),
+                                mutate_uniform(low=low, high=high, expected_num_mutations=617),
+                                ops.evaluate,
+                                ops.pool(size=pop_size),
+                                timing_probe,  # we're nice we don't include all of their extra logging in the computation
+                                *build_probes(genomes_file)  # Inserting all the probes at the end
+                            ])
+        list(ea)
+
+    print(timing_probe.buffer)
+
+    print(f"Total program time: {time.time() - beginning_time}")
---
+++ rastrigin/pygad.py
@@ -0,0 +1,73 @@
+import time
+beginning_time = time.time()
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+sys.path.insert(1, os.path.join(sys.path[0], '../gaggle'))
+from pyGAD import pygad
+import numpy as np
+
+
+times = []
+
+d = 100
+def fitness_func(solution, sol_idx):
+    return -d * len(solution) + np.sum(solution ** 2 - d * np.cos(2 * np.pi * solution))
+
+
+
+def callback_generation(ga_instance):
+    global last_gen_time, times
+    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+    cur_time = time.time()
+    time_taken = cur_time - last_gen_time
+    print(f"Time taken = {time_taken}")
+    last_gen_time = cur_time
+    times.append(time_taken)
+
+
+num_solutions = 200
+num_generations = 100
+num_parents_mating = 200
+keep_parents = 0
+keep_elitism = 0
+crossover = "uniform"
+mutation_type = "random"
+mutation_probability = 0.01
+random_mutation_min_val = -5.12
+random_mutation_max_val = 5.12
+
+ga_instance = pygad.GA(num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_func,
+                       on_generation=callback_generation,
+                       keep_parents=keep_parents,
+                       keep_elitism=keep_elitism,
+                       crossover_type=crossover,
+                       mutation_type=mutation_type,
+                       mutation_probability=mutation_probability,
+                       random_mutation_min_val=random_mutation_min_val,
+                       random_mutation_max_val=random_mutation_max_val,
+                       parent_selection_type="rws",
+                       init_range_high=5.12,
+                       init_range_low=-5.12,
+                       sol_per_pop=200,
+                       num_genes=d)
+
+last_gen_time = time.time()
+
+ga_instance.run()
+
+# After the generations complete, some plots are showed that summarize how the outputs/fitness values evolve over generations.
+ga_instance.plot_fitness(title="PyGAD & PyTorch - Iteration vs. Fitness", linewidth=4)
+
+# Returning the details of the best solution.
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+
+
+print(f"Times: {times}")
+
+print(f"Total program time: {time.time() - beginning_time}")
